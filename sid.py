b4ec69eceb579b522b5bc2e5d3f3d2f10470a1c1
Hashim Qureshi <definitelynotrushing@gmail.com>
2021-09-23 23:19:13 -0400

    File writing redundancy fixed
    One method to handle both user and artist profiles called respectively

--- sid.py
+++ sid.py
@@ -11,6 +11,23 @@ albumPath = os.path.join(exportPath, "Albums")
 cachePath = os.path.join(dirPath, ".cache")
 sp = spotipy.Spotify(auth_manager=SpotifyPKCE(client_id="72f54fa540c743268595191fc3a153d0", redirect_uri="https://webpages.uncc.edu/hquresh1/SpotifyInfoRedirect/callback/"))
 
+def writeProfileContents(account, isArtist):
+    if not isArtist:
+        file = open(os.path.join(userPath, f"{account['display_name']}.txt"), 'w')
+        file.write(f"Display Name: {user['display_name']}\n")
+    else:
+        file = open(os.path.join(artistPath, f"{account['name']}.txt"), 'w')
+        file.write(f"Name: {artist['name']}\n")
+        file.write(f"Popularity: {artist['popularity']}\n")
+
+    file.write(f"Followers: {account['followers']['total']}\n")
+    file.write(f"Type: {account['type']}\n")
+    file.write(f"ID: {account['id']}\n")
+    file.write(f"Avatar URL: {account['images'][0]['url']}\n")
+    file.write(f"Profile URL: {account['external_urls']['spotify']}\n")
+    file.write(f"Profile URI: {account['uri']}\n")
+    file.close()
+
 print("Login to your Spotify profile when using this application. (Spotify's API does not allow un-authorized requests)")
 
 choice = input("What would you like to get information for?\nYour options are: \"track\", \"album\", \"artist\", \"user\"\n\nYour Input: ")
@@ -44,16 +61,7 @@ elif choice == "artist":
     if not os.path.exists(artistPath):
         os.mkdir(artistPath)
 
-    file = open(os.path.join(artistPath, f"{artist['name']}.txt"), 'w')
-    file.write(f"Name: {artist['name']}\n")
-    file.write(f"Followers: {artist['followers']['total']}\n")
-    file.write(f"Popularity: {artist['popularity']}\n")
-    file.write(f"Type: {artist['type']}\n")
-    file.write(f"ID: {artist['id']}\n")
-    file.write(f"Artist Avatar URL: {artist['images'][0]['url']}\n")
-    file.write(f"Artist Profile URL: {artist['external_urls']['spotify']}\n")
-    file.write(f"Artist Profile URI: {artist['uri']}\n")
-    file.close()
+    writeProfileContents(artist, True)
     
     print(f"\nFinished writing all the artist's information")
     quit()
@@ -81,21 +89,10 @@ elif choice == "user":
     if not os.path.exists(userPath):
         os.mkdir(userPath)
 
-    file = open(os.path.join(userPath, f"{user['display_name']}.txt"), 'w')
-    file.write(f"Display Name: {user['display_name']}\n")
-    file.write(f"Followers: {user['followers']['total']}\n")
-    file.write(f"Type: {user['type']}\n")
-    file.write(f"ID: {user['id']}\n")
-    file.write(f"User Avatar URL: {user['images'][0]['url']}\n")
-    file.write(f"User Profile URL: {user['external_urls']['spotify']}\n")
-    file.write(f"User Profile URI: {user['uri']}\n")
-    file.close()
+    writeProfileContents(user, False)
 
     print(f"\nFinished writing all the user's information")
     quit()
 
 else:
     print("Please enter a valid selection to continue!")
-    quit()
-
-    
