eb57e2b4ed2e80f88fcc9bea854b2223bfcbb815
Hashim Qureshi <definitelynotrushing@gmail.com>
2021-09-25 11:11:11 -0400

    More adjustments (see desc.)
    - Added support for writing album information
    - Formatted output for written information with spacing
    - Checking URL input is now inside a method to prevent redundancy
    - Added sub-directory creation for albums and tracks
    - Added more comments in the code

--- sid.py
+++ sid.py
@@ -6,13 +6,18 @@ import spotipy
 from spotipy.oauth2 import SpotifyPKCE 
 import spotipy.util
 
+# Temporary import for debug purposes
+import pprint;
 
 
+
+# Fields that define where paths are as well as the spotify client
 dirPath = os.getcwd()
 exportPath = os.path.join(dirPath, "Exports")
 artistPath = os.path.join(exportPath, "Artists")
 userPath = os.path.join(exportPath, "Users")
 albumPath = os.path.join(exportPath, "Albums")
+trackPath = os.path.join(exportPath, "Tracks")
 cachePath = os.path.join(dirPath, ".cache")
 sp = spotipy.Spotify(auth_manager=SpotifyPKCE(client_id="72f54fa540c743268595191fc3a153d0", redirect_uri="https://webpages.uncc.edu/hquresh1/SpotifyInfoRedirect/callback/"))
 
@@ -21,23 +26,71 @@ sp = spotipy.Spotify(auth_manager=SpotifyPKCE(client_id="72f54fa540c743268595191
 # A method to write either user or artist information depending on what is passed in
 def writeProfileContents(account, isArtist):
     try:
+        # Artists have a 'name' and 'popularity' field inside the response object
+        # Versus users that just have 'display_name'
         if not isArtist:
             file = open(os.path.join(userPath, f"{account['display_name']}.txt"), 'w')
-            file.write(f"Display Name: {user['display_name']}\n")
+            file.write(f"Display Name: {user['display_name']}\n\n")
         else:
             file = open(os.path.join(artistPath, f"{account['name']}.txt"), 'w')
             file.write(f"Name: {artist['name']}\n")
-            file.write(f"Popularity: {artist['popularity']}\n")
+            file.write(f"Popularity: {artist['popularity']}\n\n")
 
-        file.write(f"Followers: {account['followers']['total']}\n")
-        file.write(f"Type: {account['type']}\n")
-        file.write(f"ID: {account['id']}\n")
-        file.write(f"Avatar URL: {account['images'][0]['url']}\n")
-        file.write(f"Profile URL: {account['external_urls']['spotify']}\n")
-        file.write(f"Profile URI: {account['uri']}\n")
+        file.write(f"Followers: {account['followers']['total']}\n\n")
+        file.write(f"Type: {account['type']}\n\n")
+        file.write(f"ID: {account['id']}\n\n")
+        file.write(f"Avatar URL: {account['images'][0]['url']}\n\n")
+        file.write(f"Profile URL: {account['external_urls']['spotify']}\n\n")
+        file.write(f"Profile URI: {account['uri']}\n\n")
         file.close()
     except Exception as e:
         print(f"There was an error trying to write the requested contents\nError: {e}")
+        quit()
+
+
+
+# A method to write track and album information depending on what is passed in
+def writeSongContents(content, isAlbum):
+    try:
+        file = open(os.path.join(albumPath, f"{content['name']}.txt"), 'w')
+        file.write(f"Name: {content['name']}\n\n")
+        file.write(f"Popularity: {content['popularity']}\n\n")
+        file.write(f"Release Date: {content['release_date']}\n\n")
+        file.write(f"Album Type: {content['album_type']}\n\n")
+        file.write(f"Type: {content['type']}\n\n")
+        file.write(f"ID: {content['id']}\n\n")
+        file.write(f"Is Explicit: {content['tracks']['items'][0]['explicit']}\n\n")
+        file.write(f"Artwork URL: {content['images'][0]['url']}\n\n")
+        if not isAlbum:
+            file.write(f"Preview URL: {content['preview_url']}\n\n")
+        file.write(f"Album URI: {content['uri']}\n\n")
+        file.write(f"Copyright (C): {content['copyrights'][0]['text']}\n\n")
+        file.write(f"Copyright (P): {content['copyrights'][1]['text']}\n\n")
+        file.write(f"Available Markets (ISO 3166-1 alpha-2 Code): {content['available_markets']}\n\n")
+        file.close()
+    except Exception as e:
+        print(f"There was an error trying to write the requested contents\nError: {e}")
+        quit()
+
+
+
+# A method to read the users input for the URL and format it
+def checkInput(choice):
+    url = input(f"Enter URL of {choice} here: ")
+    try:
+        beginIndex = url.rfind('/') + 1
+        endIndex = url.index("?si")
+        if not choice == "artist":
+            url = url[beginIndex:endIndex]
+        else:
+            url = f"spotify:{choice}:{url[beginIndex:endIndex]}"
+    except ValueError as ve:
+        print(f"Please enter in a proper Spotify URL to continue!\nError: {ve}")
+        quit()
+    except Exception as e:
+        print(f"There was an error trying to format the URL\nError: {e}")
+        quit()
+    return url
 
 
 
@@ -49,34 +102,50 @@ try:
         os.mkdir(artistPath)
     if not os.path.exists(userPath):
         os.mkdir(userPath)
+    if not os.path.exists(albumPath):
+        os.mkdir(albumPath)
+    if not os.path.exists(trackPath):
+        os.mkdir(trackPath)
 except Exception as e:
     print(f"There was an error trying to create the \"Exports\" folder\nError: {e}")
     quit()
 
 # Display prompt to the user and read the choice they make
 print("Login to your Spotify profile when using this application. (Spotify's API does not allow un-authorized requests)")
-
 choice = input("What would you like to get information for?\nYour options are: \"track\", \"album\", \"artist\", \"user\"\n\nYour Input: ")
 choice = choice.lower()
 
 # Depending on what the user wants, the program will perform the task
 if choice == "track":
-    quit() # Not implemented yet
+    url = checkInput(choice)
+
+    try:
+        track = sp.track(url)
+    except Exception as e:
+        print(f"There was an error trying to get the track information\nError: {e}")
+        if os.path.exists(cachePath):
+            os.remove(cachePath)
+        quit()
+    
+    pprint.pprint(track)
 elif choice == "album":
-    quit() # Not implemented yet
+    url = checkInput(choice)
+
+    try:
+        album = sp.album(url)
+    except Exception as e:
+        print(f"There was an error trying to get the track information\nError: {e}")
+        if os.path.exists(cachePath):
+            os.remove(cachePath)
+        quit()
+    
+    writeSongContents(album, True)
+
+    print(f"\nFinished writing all the album's information")
+    quit()
 elif choice == "artist":
     # Get the URL of the artist and format it into a URI
-    url = input("Enter URL of artist here: ")
-    try:
-        beginIndex = url.rfind('/') + 1
-        endIndex = url.index("?si")
-        url = f"spotify:{choice}:{url[beginIndex:endIndex]}"
-    except ValueError as ve:
-        print(f"Please enter in a proper Spotify URL to continue!\nError: {ve}")
-        quit()
-    except Exception as e:
-        print(f"There was an error trying to format the URL\nError: {e}")
-        quit()
+    url = checkInput(choice)
     
     # Get the artist from the URI above to obtain information for
     try:
@@ -87,32 +156,25 @@ elif choice == "artist":
             os.remove(cachePath)
         quit()
 
-    # Calls the method that is responsible 
+    # Calls the method that will write the artist information to file
     writeProfileContents(artist, True)
     
     print(f"\nFinished writing all the artist's information")
     quit()
 elif choice == "user":
-    url = input("Enter URL of user here: ")
-    try:
-        beginIndex = url.rfind('/') + 1
-        endIndex = url.index("?si")
-        url = url[beginIndex:endIndex]
-    except ValueError as ve:
-        print(f"Please enter in a proper Spotify URL to continue!\nError: {ve}")
-        quit()
-    except Exception as e:
-        print(f"There was an error trying to format the URL\nError: {e}")
-        quit()
+    # Get the URL of the user profile and format it into a URI
+    url = checkInput(choice)
 
+    # Get the artist from the URI above to obtain information for
     try:
         user = sp.user(url)
     except Exception as e:
-        print(f"There was an error trying to get the artist information\nError: {e}")
+        print(f"There was an error trying to get the user profile information\nError: {e}")
         if os.path.exists(cachePath):
             os.remove(cachePath)
         quit()
 
+    # Calls the method that will write the user information to file
     writeProfileContents(user, False)
 
     print(f"\nFinished writing all the user's information")
